304034529b6d76a9662f545bb0e8acff1360e5f6
Angel Valkov <angel4o2003@abv.bg>
2025-09-26 00:40:28 +0300

    notes component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import toast from "react-hot-toast";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
+import NoteCard from "../components/NoteCard";
 import axios from "axios";
 
 
@@ -45,14 +46,11 @@ const HomePage = () => {
         {loading && <div className="text-center text-primary py-10">Loading notes ...</div>}
 
         {notes.length > 0 && !isRateLimited &&(
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-
-            {notes.map(note => (
-              <div>
-                {note.title} | {note.content}
-              </div>
-            ))}
-          </div>
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    {notes.map((note) => (
+      <NoteCard key={note._id} note={note} setNotes={setNotes} />
+    ))}
+  </div>
         )}
       </div>
       <h1>Home Page</h1>
