05b8550ca0feb0ca5dd7cafba0eb58f910138edf
Angel Valkov <angel4o2003@abv.bg>
2025-09-25 22:30:13 +0300

    Fetching data from backend, navbar, lading and request limit screen

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,60 @@
-import React from "react";
-import toast from "react-hot-toast"
+import React, { useState, useEffect } from "react";
+import toast from "react-hot-toast";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import axios from "axios";
+
+
 
 const HomePage = () => {
+  const [isRateLimited, setRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchNotes = async() => {
+      try{
+        const res = await axios.get("http://localhost:5001/api/notes")
+        console.log(res.data);
+        setNotes(res.data);
+        setRateLimited(false)
+
+
+      }
+      catch(error){
+        console.log("Error fetching", error)
+                if(error.response.status === 429){
+          setRateLimited(true)
+        } else{
+          toast.error("Failed to load notes")
+        }
+      } finally{
+        setLoading(false)
+      }
+    }
+
+    fetchNotes();
+  }, [])
   return (
-    <div>
+    <div className="min-h-screen">
+      <Navbar/>
+
+      {isRateLimited && <RateLimitedUI/>}
+
+      <div className="max-w-7x1 mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes ...</div>}
+
+        {notes.length > 0 && !isRateLimited &&(
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+
+            {notes.map(note => (
+              <div>
+                {note.title} | {note.content}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
       <h1>Home Page</h1>
       <button className="btn btn-primary" onClick={() => toast.success("gg")}>Click me</button>
     </div>
