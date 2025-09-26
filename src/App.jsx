0ad0417666ae7d6c048e85fe62e1bb2cf6771394
Angel Valkov <angel4o2003@abv.bg>
2025-09-26 12:21:49 +0300

    Create page

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ import NoteDetailPage from "./pages/NoteDetailPage"
 
 const App = () => {
   return (
+        <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#FFF_60%,#5768FF_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
+          </div>
   )
 }
 
